e0c2264bef750e6d758f290e49a4a534a38cde1e
Julián Fernando Salamanca Chacón <105863909+jsalamanca3@users.noreply.github.com>
2023-09-29 14:06:52 -0500

    cargando correcciones de router views y realTimeProducts

--- src/public/js/realTimeProducts.js
+++ src/public/js/realTimeProducts.js
@@ -1,50 +1,43 @@
-// main.js
-const socket = io();
+const socket = io('/api/');
 const productList = document.getElementById('productList');
 const addProductForm = document.getElementById('addProductForm');
 const deleteProductForm = document.getElementById('deleteProductForm');
+
 productList.addEventListener('click', (e) => {
   if (e.target.classList.contains('delete-button')) {
     const productId = e.target.dataset.productId;
     socket.emit('deleteProduct', productId);
   }
 });
+
 socket.on('productAdded', (newProduct) => {
   const newItem = document.createElement('li');
   newItem.textContent = newProduct.title;
   newItem.dataset.productId = newProduct.id;
   productList.appendChild(newItem);
 });
+
 socket.on('productDeleted', (productId) => {
   const productItem = document.querySelector(`li[data-product-id="${productId}"]`);
   if (productItem) {
     productItem.remove();
   }
 });
+
 addProductForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const productTitleInput = addProductForm.querySelector('[name="title"]');
   const newProduct = {
-    name: productTitleInput.value,
+    title: productTitleInput.value,
   };
   socket.emit('addProduct', newProduct);
   productTitleInput.value = '';
 });
+
 deleteProductForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const productIdInput = deleteProductForm.querySelector('[name="id"]');
   const productId = productIdInput.value;
   socket.emit('deleteProduct', productId);
   productIdInput.value = '';
-});
-
-fetch('/addProduct', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify({
-    name: 'Producto 1',
-    price: 100,
-  }),
 });
--- src/router/views.router.js
+++ src/router/views.router.js
@@ -13,12 +13,23 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/realtimeproducts", (req, res) => {
+router.get("/realTimeProducts", (req, res) => {
   try {
     const products = productManager.getProducts();
     res.render('realTimeProducts', { products });
   } catch (error) {
     console.error("Error al obtener la lista de productos:", error);
+    res.status(500).send("Error interno del servidor");
+  }
+});
+
+router.post("/realTimeProducts/addProduct", async (req, res) => {
+  try {
+    const product = req.body;
+    const newProduct = await productManager.addProduct(product);
+    res.status(201).json(newProduct);
+  } catch (error) {
+    console.error("Error al agregar un producto:", error);
     res.status(500).send("Error interno del servidor");
   }
 });
