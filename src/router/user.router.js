e4a4c40e3dfde66a27ad1c992f7f16828ee40939
Julián Fernando Salamanca Chacón <105863909+jsalamanca3@users.noreply.github.com>
2023-12-30 07:22:09 -0500

    cargando implementacion de EndPoint para cambiar de rol user a premium y viceversa

--- src/router/user.router.js
+++ src/router/user.router.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { usersModel } from "../persistencia/dao/models/users.model.js";
 import { usersManager } from "../persistencia/dao/managers/userManager.js";
 import bcrypt from "bcrypt";
 import passport from "passport";
@@ -80,18 +81,29 @@ router.post("/", async (req, res) => {
 });
 
 router.put('/premium/:uid', async (req, res) => {
+  logger.info('Entrando al endpoint: premiun.uid');
   const { uid } = req.params;
   try {
     const user = await usersModel.findById(uid);
+
     if (!user) {
       return res.status(404).json({ error: errorDictionary['USER_NOT_FOUND'] });
     }
-    user.role = user.role === 'user' ? 'premium' : 'user';
+
+    const newRole = user.role === 'user' ? 'premium' : 'user';
+
+    if (newRole !== 'user' && newRole !== 'premium') {
+      return res.status(400).json({ error: 'Rol no válido' });
+    }
+
+    user.role = newRole;
     await user.save();
     res.status(200).json({ message: 'Rol de usuario actualizado con éxito' });
   } catch (error) {
+    logger.error('Error al cambiar el rol:', error);
     res.status(500).json({ error: errorDictionary['INTERNAL_SERVER_ERROR'] });
   }
 });
+
 
 export default router;
